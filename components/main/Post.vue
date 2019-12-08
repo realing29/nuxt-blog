f6d52a1dacbbfa8896ef1a9b82e3a34c015b11b5
realing29 <realing29@gmail.com>
2019-12-08 16:49:03 +0500

    Пройден 20 урок, создание главной страницы

---
+++ components/main/Post.vue
@@ -0,0 +1,18 @@
+<template>
+  <article class="post">
+    
+  </article>
+</template>
+
+<script>  
+export default {
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+  .post
+    height 200px
+    border 1px solid black
+    margin-bottom 2rem
+</style>
