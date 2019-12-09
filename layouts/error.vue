c60cb6d375360aa2bd35624842f41accc36fa5a8
realing29 <realing29@gmail.com>
2019-12-10 02:39:50 +0500

    Пройден 23 урок "обработка ошибки роутов"

---
+++ layouts/error.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error-layout">
+    <div class="error-content">
+      <h1 class="error-title">Ошибка 404</h1>
+      <nuxt-link to='/'>Вернуться на гравную</nuxt-link>
+    </div>
+  </div>
+</template>
+<style lang="stylus" scoped>
+  .error-layout
+    display flex
+    justify-content center
+    align-items center
+    width 100%
+  .error-content
+    display flex
+    flex-direction column
+    align-items center
+    padding-top 4rem
+  .error-title
+    color rgb(145, 6, 6)
+    margin-bottom 1rem
+</style>
