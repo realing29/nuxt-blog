0f96fbc3911931e339364169b34634c8735ad04d
realing29 <realing29@gmail.com>
2019-12-13 22:12:19 +0500

    Пройден 24 урок - "Список комментариев"

---
+++ components/main/Comment.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-card class="comment">
+    <div class="comment-header">
+      <span>Person name</span>
+      <small>
+        <i class="el-icon-time"></i>
+        {{ new Date().toLocaleString() }}
+      </small>
+    </div>
+    <div class="comment-text">
+      Lorem ipsum dolor sit amet.
+    </div>
+  </el-card>
+</template>
+
+<script>
+export default {
+  props: ['comment']
+};
+</script>
+
+<style lang="stylus" scoped>
+  .comment
+    margin-bottom 1rem
+  .comment-header
+    display flex
+    justify-content space-between
+    margin-bottom 0.5rem
+    font-size 0.8rem
+  .comment-text
+    white-space pre-line
+</style>
--- pages/post/_id.vue
+++ pages/post/_id.vue
@@ -30,16 +30,27 @@
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus eveniet aliquam corrupti tempora soluta nam, voluptatum doloremque, quos incidunt quas molestiae adipisci voluptas, dolore mollitia deleniti facere qui nemo culpa!</p>
     </main>
     <footer>
-      
+      <!-- Form -->
+
+      <div class="comments" v-if='false'>
+        <app-comment
+          v-for="comment in 4"
+          :key="comment"
+          :comment='comment'
+        />
+      </div>
+      <div class="text-center" v-else>Комментариев нет</div>
     </footer>
   </article>
 </template>
 
 <script>
+import AppComment from '@/components/main/Comment'
 export default {
   validate({ params }) {
     return Boolean(params.id);
-  }
+  },
+  components: {AppComment}
 };
 </script>
 <style lang="stylus" scoped>
