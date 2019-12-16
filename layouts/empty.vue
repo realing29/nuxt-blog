788455859b1746bf138f8edb7065c4249977c5ac
realing29 <realing29@gmail.com>
2019-12-17 00:38:21 +0500

    Пройден 30 урок "Страница логина"

---
+++ layouts/empty.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="empty-layout">
+    <nuxt />
+  </div>
+</template>
+
+<style lang="stylus" scoped>
+  .empty-layout
+    display flex
+    justify-content center
+    width 100%
+    padding-top 4rem
+</style>
