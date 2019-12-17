d0b25fac2fbac97dcbd85666a8fe31c2e9987a3d
realing29 <realing29@gmail.com>
2019-12-17 06:04:08 +0500

    Пройден 33 урок "завершение сессии"

--- pages/admin/logout.vue
+++ pages/admin/logout.vue
@@ -1,10 +1,14 @@
 <template>
-  <div>logout</div>
+  <div>Очищаем данные</div>
 </template>
 
 <script>
 export default {
   layout: 'admin',
-  middleware: ['admin-auth']
+  middleware: ['admin-auth'],
+  beforeCreate(){
+    this.$store.dispatch('auth/logout')
+    this.$router.push('/admin/login?message=logout')
+  }
 }
 </script>
