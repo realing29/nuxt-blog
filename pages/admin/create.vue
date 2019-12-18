72e6de98cca74aa1d02d8f8897604ec0baf2e9ae
realing29 <realing29@gmail.com>
2019-12-19 04:36:09 +0500

    Пройден урок 41 и 4 раздел "Загрузка файла"

--- pages/admin/create.vue
+++ pages/admin/create.vue
@@ -18,6 +18,19 @@
       </div>
     </el-dialog>
 
+    <el-upload
+      class="mb"
+      drag
+      ref='upload'
+      action="https://jsonplaceholder.typicode.com/posts/"
+      :on-change="handleImageChange"
+      :auto-upload="false"
+    >
+      <i class="el-icon-upload"></i>
+      <div class="el-upload__text">перетащите картинку <em>или нажмите</em></div>
+      <div class="el-upload__tip" slot="tip">файлы с расширением jpg/png</div>
+    </el-upload>
+
     <el-form-item>
       <el-button type="primary" native-type="submit" round :loading="loading">Создать пост</el-button>
     </el-form-item>
@@ -30,6 +43,7 @@ export default {
   middleware: ["admin-auth"],
   data() {
     return {
+      image: null,
       previewDialog: false,
       loading: false,
       controls: {
@@ -55,25 +69,33 @@ export default {
     };
   },
   methods: {
+    handleImageChange(file, fileList){
+      this.image = file.raw
+    },
     onSubmit() {
       this.$refs.form.validate(async valid => {
-        if (valid) {
+        if (valid && this.image) {
           this.loading = true;
 
           const formData = {
             title: this.controls.title,
-            text: this.controls.text
+            text: this.controls.text,
+            image: this.image
           };
 
           try {
             await this.$store.dispatch("post/create", formData);
-            this.controls.text = "";
-            this.controls.title = "";
+            this.controls.text = ""
+            this.controls.title = ""
+            this.image = null
+            this.$refs.upload.clearFiles()
             this.$message.success("Пост создан");
           } catch (e) {
           } finally {
             this.loading = false;
           }
+        } else {
+          this.$message.warning('Форма не валидна')
         }
       });
     }
