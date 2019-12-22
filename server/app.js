52418f47ffb38bf1a31b735c7b5b3dbcb1184338
realing29 <realing29@gmail.com>
2019-12-22 08:14:59 +0500

    Пройден урок 45 "Роуты, контроллеры, модели"

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const authRoutes = require('./routes/auth.routes')
 const keys = require('./keys')
 const app = express()
 
@@ -10,5 +11,7 @@ mongoose.connect(keys.MONGO_URI)
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
+
+app.use('/api/auth', authRoutes)
 // Здесь пишем логику для бекенда
 module.exports = app
