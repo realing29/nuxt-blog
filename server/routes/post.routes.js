19086f80de4d3aff4a472fc8af57ee6709c25871
realing29 <realing29@gmail.com>
2019-12-24 08:06:11 +0500

    Пройден урок "50 роуты постов"

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,7 +1,45 @@
 const passport = require('passport')
 const {Router} = require('express')
+const ctr = require('../controllers/post.controller')
 const router = Router()
 
-// /api.post
+
+// Admin
+// /api.post/admin
+router.post(
+  '/admin/',
+  passport.authenticate('jwt', {session: false}),
+  ctr.create
+)
+
+router.get(
+  '/admin/',
+  passport.authenticate('jwt', { session: false }),
+  ctr.getAll
+)
+
+router.get(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.getById
+)
+
+router.put(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.update
+)
+
+router.delete(
+  '/admin/:id',
+  passport.authenticate('jwt', { session: false }),
+  ctr.remove
+)
+
+// Base
+// /api.post/a
+router.get('/', ctr.getAll)
+router.get('/:id', ctr.getById)
+router.put('/:id', ctr.addView)
 
 module.exports = router
