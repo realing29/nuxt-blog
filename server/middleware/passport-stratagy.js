339b337094a97962995ee8f6b4de1350d53db8e3
realing29 <realing29@gmail.com>
2019-12-29 13:05:49 +0500

    Завершен раздел 5 урок 54 "Исправление ошибки"

--- server/middleware/passport-stratagy.js
+++ server/middleware/passport-stratagy.js
@@ -1,7 +1,7 @@
 const {Strategy, ExtractJwt} = require('passport-jwt')
-const {model} = require('mongoose')
+// const {model} = require('mongoose')
 const keys = require('../keys')
-const User = model('users')
+const User = require('..models/user.model')//model('users')
 
 const options = {
   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
